207edd50d9e27ee175da7f7c56fff6a8ef818fd7
SanjaIgnova <118814193+SanjaIgnova@users.noreply.github.com>
2023-03-30 09:36:46 +0200

    Add files via upload

---
+++ SEDC.Classo1/SEDC.Class01.Exercises/NewFolder/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Class01.Exercises.NewFolder
+{
+    public enum Game
+    {
+        Rock = 1,
+        Paper,
+        Scissors
+    }
+}
